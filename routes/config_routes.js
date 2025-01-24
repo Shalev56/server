e4ecfa7b13450437900dc5e01f5ace17e198bc26
Shalev56 <shalevu56@gmail.com>
2025-01-24 13:06:39 +0200

    Delete routes directory

--- routes/config_routes.js
+++
@@ -1,9 +0,0 @@
-const indexR = require("./index");
-const usersR = require("./users");
-const countriesR = require("./countries");
-
-exports.routesInit = (app) => {
-  app.use("/",indexR);
-  app.use("/users",usersR);
-  app.use("/countries",countriesR);
-}
