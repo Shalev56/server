9819f3627b52a250ca55d88386bcf3709b32a7d8
Shalev Uziel <shalevu56@gmail.com>
2025-01-29 12:36:29 +0200

    server

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,6 +5,9 @@ const { confsecretList } = require('../config/secret.js');
 // middleware
 exports.auth = async(req,res,next) => {
   let token = req.header("x-api-key")
+  if(!token){
+    return res.status(401).json({msg:"You need to send token to this endpoint url 66666"})
+  }
   try{
     // מנסה לפענח את הטוקן ויכיל את כל המטען/מידע שבתוכו
     // ובמיוחד את האי די
@@ -18,5 +21,6 @@ exports.auth = async(req,res,next) => {
   }
   catch(err){
     console.log(err); // Log the error to understand the issue
+   return res.status(401).json({msg:"Token not valid or expired 77777777"})
   }
 }
